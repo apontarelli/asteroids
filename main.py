ebbd29350e1633fb24e9bc39b2419cd49be3f252
apontarelli <antoniopontarelli@gmail.com>
2025-01-12 15:34:19 -0800

    "Add split method to asteroid"

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if asteroid.collision_check(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         for o in drawable:
             o.draw(screen)
